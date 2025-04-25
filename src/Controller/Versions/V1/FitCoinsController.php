35e60310df61aeef7efa7f5756233cec7f5e3905
DevJamesOnePayments <james@onepayments.net>
2025-04-25 19:03:15 -0300

    att

---
+++ src/Controller/Versions/V1/FitCoinsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Versions\V1;
+
+use App\Helpers\AuthHelpers;
+use App\Repository\PacotesFitCoinsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/v1/fit-coins')]
+final class FitCoinsController extends AbstractController
+{
+
+    private $authHelpers;
+    private $serializer;
+    public function __construct(AuthHelpers $authHelpers, SerializerInterface $serializer)
+    {
+      $this->authHelpers = $authHelpers;
+      $this->serializer = $serializer;
+    }
+
+    #[Route('/', methods:['GET'])]
+    public function index(PacotesFitCoinsRepository $pacotesFitCoinsRepository): JsonResponse
+    {
+
+        $this->authHelpers->is_autenticado();
+
+        $result = $pacotesFitCoinsRepository->findAll();
+
+        $data = array_map(fn($item) => [
+            'id' => $item->getId(),
+            'Titulo' => $item->getTitulo(),
+            'Descricao' => $item->getDescricao(),
+            'Valor' => $item->getValor(),
+            'QtdCoins' => $item->getQtdCoins(),
+        ], $result);
+
+        return $this->json($data, 200);
+    }
+}
