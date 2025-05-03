1f6045a2126fb65e3b23fd8cf757c559b74a3af0
DevJamesOnePayments <james@onepayments.net>
2025-05-03 00:33:58 -0300

    Testando novamente

--- src/Controller/Teste.php
+++ src/Controller/Teste.php
@@ -1,1 +1,55 @@
-Teste 2 
+<?php 
+
+namespace App\Controller;
+
+use App\Service\Payments\MercadoPagoService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+#[Route('/test')]
+final class Teste extends AbstractController
+{
+    #[Route('/mercado-pago', methods:['POST'])]
+    public function index(Request $request, MercadoPagoService $mercadoPagoService): JsonResponse
+    {
+        $data = ($request->headers->get('Content-Type') === 'application/json')
+        ? $request->toArray()
+        : $request->request->all();
+
+        $idPagamento = $data['data']['id'];
+
+        $mercadoPagoService->buscarIdPagamento($idPagamento);
+
+        return $this->json(['status' => 'received'], 200);
+    }
+
+    #[Route('/mercado-pago/checkout-transparente', methods:['POST'])]
+    public function checkoutTransparente(Request $request, MercadoPagoService $mercadoPagoService): JsonResponse
+    {
+        $data = ($request->headers->get('Content-Type') === 'application/json')
+        ? $request->toArray()
+        : $request->request->all();
+
+        $idPagamento = $data['data']['id'];
+
+        $mercadoPagoService->buscarIdPagamento($idPagamento);
+
+        return $this->json(['status' => 'received'], 200);
+    }
+
+    /*#[Route('/mercado-pago/test/status/{id}', methods:['POST'])]
+    public function teststatus($id, Request $request, MercadoPagoService $mercadoPagoService): JsonResponse
+    {
+        $data = ($request->headers->get('Content-Type') === 'application/json')
+        ? $request->toArray()
+        : $request->request->all();
+
+        $select = $mercadoPagoService->buscarIdPagamento($id);
+        
+
+        return $this->json(['result' => $select], 200);
+    }*/
+
+}
